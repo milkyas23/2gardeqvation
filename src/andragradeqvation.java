ea544ee835a5f3718f4548dcbf83032f3c480128
MilkyasEyob.Habte <milkyas23>
2019-10-25 11:21:23 +0200

    start på andragrad eqvation.

--- src/andragradeqvation.java
+++ src/andragradeqvation.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class andragradeqvation {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+
+        System.out.println("Ange a: ");
+        a = input.nextDouble ();
+        System.out.println("Ange b: ");
+        b = input.nextDouble ();
+        System.out.println("Ange c: ");
+        c = input.nextDouble();
+
+        double a;
+        double b;
+        double c;
+
+
+    }
+
 }
